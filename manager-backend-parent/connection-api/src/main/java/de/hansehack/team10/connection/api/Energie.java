64b52bac5ab8a738f8245d0ed7479ae951882b59
Sven Marquardt <sven2q@hotmail.de>
2018-09-06 20:37:46 +0200

    Klasse um nach Energie
    klasse um nach energie in einem zeitrahmen zu fragen
    
    Signed-off-by: Sven Marquardt <sven2q@hotmail.de>

---
+++ manager-backend-parent/connection-api/src/main/java/de/hansehack/team10/connection/api/Energie.java
@@ -0,0 +1,39 @@
+package de.hansehack.team10.connection.api;
+
+import java.time.Duration;
+
+public class Energie {
+	/**
+	 * Energie needed per Hour
+	 */
+	private final int energieAmount;
+	/**
+	 * How long will the energie be needed.
+	 */
+	private final Duration energiePerHour;
+	/**
+	 * @param energieAmount
+	 * @param energiePerHour
+	 */
+	public Energie(final int energieAmount, final Duration energiePerHour) {
+		super();
+		this.energieAmount = energieAmount;
+		this.energiePerHour = energiePerHour;
+	}
+	/**
+	 * @return the energieAmount
+	 */
+	public int getEnergieAmount() {
+		return this.energieAmount;
+	}
+	/**
+	 * @return the energiePerHour
+	 */
+	public Duration getEnergiePerHour() {
+		return this.energiePerHour;
+	}
+	
+	
+	
+
+}
