dc8d3ee2c02b8ca769447f147cc0d9b571ac2249
Sven Marquardt <sven2q@hotmail.de>
2018-09-06 18:38:18 +0200

    Simulierte nutzer
    Signed-off-by: Sven Marquardt <sven2q@hotmail.de>

---
+++ manager-backend-parent/ssimulated-clients/src/main/java/de/hansehack/team10/simulated/clients/SimulatedClient.java
@@ -0,0 +1,16 @@
+package de.hansehack.team10.simulated.clients;
+
+import java.util.List;
+
+import de.hansehack.mqtt.client.Client;
+
+public class SimulatedClient {
+	
+	private Client mqttClient;
+	
+	private List<String> topics;
+	
+	
+	
+
+}
