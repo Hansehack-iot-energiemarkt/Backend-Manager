eff5f3db330c119101e3fb0d310d90ddab3bc901
Sven Marquardt <sven2q@hotmail.de>
2018-09-06 22:43:50 +0200

    Simulation für die simulierten clients
    Signed-off-by: Sven Marquardt <sven2q@hotmail.de>

---
+++ manager-backend-parent/ssimulated-clients/src/main/java/de/hansehack/team10/simulated/clients/SimulatdMain.java
@@ -0,0 +1,93 @@
+package de.hansehack.team10.simulated.clients;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.Consumer;
+import java.util.stream.IntStream;
+
+import org.eclipse.paho.client.mqttv3.MqttAsyncClient;
+import org.eclipse.paho.client.mqttv3.MqttClientPersistence;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+import de.hansehack.mqtt.client.Client;
+import de.hansehack.team10.connection.api.Connection;
+import de.hansehack.team10.connection.api.Energie;
+import de.hansehack.team10.connection.api.Message;
+import de.hansehack.team10.connection.api.Task;
+import de.hansehack.team10.connection.api.Topic;
+
+public class SimulatdMain {
+	private final static String BROKER= "tcp:://10.250.252.184";
+	private final static String DEVICE="device";
+	private final static MqttClientPersistence store = new MemoryPersistence();
+	private static final List<SimulatedClient> clients = new ArrayList<>();
+	private static final List<String> devices= new ArrayList<>();
+	private static Map<Message, Consumer<Message>> tasks = new LinkedHashMap<>();
+	private static List<Timer> timers = new ArrayList<>();
+	public static void main(final String[] args) {
+		
+		try {
+			final Connection mqttConnection = new Client(SimulatdMain.BROKER, MqttAsyncClient.generateClientId(), SimulatdMain.store);
+			final String deviceName = SimulatdMain.DEVICE+SimulatdMain.devices.size()+1;
+			SimulatdMain.devices.add(deviceName);
+			final SimulatedClient firstClient = new SimulatedClient(mqttConnection, SimulatdMain.generateStartupTasks(deviceName), SimulatdMain.generateEnergie(deviceName), deviceName);
+			SimulatdMain.clients.add(firstClient);
+			final String deviceNameSecond = SimulatdMain.DEVICE+SimulatdMain.devices.size()+1;
+			SimulatdMain.devices.add(deviceNameSecond);
+			final SimulatedClient secondClient = new SimulatedClient(mqttConnection, SimulatdMain.generateStartupTasks(deviceName), SimulatdMain.generateEnergie(deviceName), deviceName);
+			SimulatdMain.clients.add(secondClient);
+			
+		} catch (final MqttException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		SimulatdMain.clients.forEach(client->{
+			final Timer timer = new Timer(client.getName());
+			timer.schedule(client, ThreadLocalRandom.current().nextLong());
+			SimulatdMain.timers.add(timer);
+		});
+		final Scanner scanner = new Scanner(System.in);
+		final String key = "";
+		System.out.println("Press any key to exit");
+		while (key.isEmpty()) {
+			scanner.nextLine();
+			
+		}
+	}
+	
+	private static List<Message> generateStartupTasks(final String deviceName){
+		final List<Message> messages = new ArrayList<>();
+		
+		IntStream.range(0, 3).forEach(i->{
+			final int hours = ThreadLocalRandom.current().nextInt(1, 25);
+			final int energieAmount = ThreadLocalRandom.current().nextInt(1, Integer.MAX_VALUE);
+			final Task task = new Task(Duration.ofHours(hours), energieAmount);
+			final Message message = new Message(deviceName, task,Topic.TaskNew);
+			SimulatdMain.tasks.put(message, (canConsume)->{
+				System.out.println("Maschine "+deviceName+" darf folgenden task zuende führen: "+task);
+			});
+			messages.add(message);
+		});
+		return messages;
+	}
+	
+	private static List<Message> generateEnergie(final String deviceName) {
+		final List<Message> messages = new ArrayList<>();
+		IntStream.range(0, 3).forEach(i->{
+			final int energieAmount = ThreadLocalRandom.current().nextInt(1, Integer.MAX_VALUE);
+			final Duration energiePerHour = Duration.ofHours(ThreadLocalRandom.current().nextLong(3600,24*3600+1));
+			final Energie energie = new Energie(energieAmount, energiePerHour);
+			final Message message = new Message(deviceName, energie, Topic.Energie);
+			messages.add(message);
+		});
+		return messages;
+	}
+
+}
