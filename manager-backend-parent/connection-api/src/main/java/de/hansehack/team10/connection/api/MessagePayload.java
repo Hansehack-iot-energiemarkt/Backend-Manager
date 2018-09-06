769b5dc46be734f09be4cb6ca1acbaa38ca564c9
Sven Marquardt <sven2q@hotmail.de>
2018-09-06 21:41:37 +0200

    Arbeite mit subtypen
    Signed-off-by: Sven Marquardt <sven2q@hotmail.de>

--- manager-backend-parent/connection-api/src/main/java/de/hansehack/team10/connection/api/MessagePayload.java
+++ manager-backend-parent/connection-api/src/main/java/de/hansehack/team10/connection/api/MessagePayload.java
@@ -1,47 +1,6 @@
 package de.hansehack.team10.connection.api;
 
-import java.util.Optional;
-
-import javax.annotation.Nullable;
-
-final public class MessagePayload {
-	/**
-	 * Energie message
-	 */
-	private final Optional<Energie> energie;
-	/**
-	 * Task message
-	 */
-	private final Optional<Task> task;
-	/**
-	 * @param energie
-	 * @param task
-	 */
-	public MessagePayload(@Nullable final Energie energie,@Nullable final Task task) {
-		super();
-		this.energie = Optional.ofNullable(energie);
-		this.task = Optional.ofNullable(task);
-		if(!this.energie.isPresent()&&!this.task.isPresent()) {
-			throw new IllegalArgumentException("Einer der beiden Objekte muss present sein"); 
-		}
-	}
-	/**
-	 * @return the energie
-	 */
-	public Optional<Energie> getEnergie() {
-		return this.energie;
-	}
-	/**
-	 * @return the task
-	 */
-	public Optional<Task> getTask() {
-		return this.task;
-	}
-	
-	
-	
-	
-	
+abstract public class MessagePayload {
 	
 	
 }
