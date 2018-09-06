e0e8aac74d5480c721023bef65a9bf9002d73100
Sven Marquardt <sven2q@hotmail.de>
2018-09-06 18:42:43 +0200

    Messages erhalten in String über getMessage
    Signed-off-by: Sven Marquardt <sven2q@hotmail.de>

--- manager-backend-parent/connection-api/src/main/java/de/hansehack/team10/connection/api/Message.java
+++ manager-backend-parent/connection-api/src/main/java/de/hansehack/team10/connection/api/Message.java
@@ -2,11 +2,33 @@ package de.hansehack.team10.connection.api;
 
 public class Message {
 	
-	private String deviceName;
+	private final String deviceName;
 	
-	private MessagePayload message;
+	private final MessagePayload message;
 	
-	private String topic;
+	private final String topic;
+	
+	
+	
+	
+	/**
+	 * @param deviceName
+	 * @param message
+	 * @param topic
+	 */
+	public Message(final String deviceName, final MessagePayload message, final String topic) {
+		super();
+		this.deviceName = deviceName;
+		this.message = message;
+		this.topic = topic;
+	}
+
+
+
+
+	public String getMessage() {
+		return this.deviceName+":"+this.message.getMessage()+":"+this.topic;
+	}
 	
 	
 	
--- manager-backend-parent/connection-api/src/main/java/de/hansehack/team10/connection/api/MessagePayload.java
+++ manager-backend-parent/connection-api/src/main/java/de/hansehack/team10/connection/api/MessagePayload.java
@@ -1,5 +1,29 @@
 package de.hansehack.team10.connection.api;
 
 public class MessagePayload {
+	
+	private final String message;
+	
+	
+	
+	
+	
+	
+	/**
+	 * @param message
+	 */
+	public MessagePayload(final String message) {
+		super();
+		this.message = message;
+	}
+
+
+
+
+
+
+	public String getMessage() {
+		return this.message;
+	}
 
 }
