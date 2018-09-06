97d7e65f8eb8f2a1574e3c6e598ab1fedfe64590
Sven Marquardt <sven2q@hotmail.de>
2018-09-06 16:55:34 +0200

    Interface für die einzelnen Verbindungen
    Wie sollen Nachrichten über verschiedene Verbindungen weiter gegeben
    werden. Subscriber und publisher methoden für die buisness logik
    
    Signed-off-by: Sven Marquardt <sven2q@hotmail.de>

---
+++ manager-backend-parent/connection-api/src/main/java/de/hansehack/team10/connection/api/Connection.java
@@ -0,0 +1,24 @@
+package de.hansehack.team10.connection.api;
+
+import java.util.function.Consumer;
+
+/**
+ * Dieses Interface muss von allen Klassen implementiert werden, die vom
+ * Backendmanager angesprochen werden sollen.
+ * 
+ * @author eiamnacken	
+ *
+ */
+public interface Connection {
+	/**
+	 * Wird augerufen jedesmal wenn eine Nachricht erhalten wurde
+	 * @param messageConsumer
+	 */
+	void messageReceivedSubscriber(Consumer<Message> messageConsumer);
+	
+	/**
+	 * Wird aufgerufen wenn eine Nachricht verschickt werden soll.
+	 */
+	Consumer<Message> messageSend();
+
+}
---
+++ manager-backend-parent/connection-api/src/main/java/de/hansehack/team10/connection/api/MessagePayload.java
@@ -0,0 +1,5 @@
+package de.hansehack.team10.connection.api;
+
+public class MessagePayload {
+
+}
